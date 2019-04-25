177851da707f9b8174f05eee6027bf5270ef7da9
Oleh SLOBODANIUK <osloboda@e1r6p12.unit.ua>
2019-04-25 21:10:22 +0300

    add 3 floor

--- start.py
+++ start.py
@@ -98,14 +98,17 @@ class Map(tk.Frame):
         self.leftpanel_switch.pack(side=TOP)
 
         self.floor_switch = 1
-
+        self.button_r = tk.Button(self.leftpanel, state=NORMAL, command=lambda:
+        refresh(self), text='Refresh', bd=0)
+        self.button_r.pack(side=TOP, fill=tk.X)
         self.button_s_1 = tk.Button(self.leftpanel_switch, state=NORMAL, command=lambda:
         floor_switch(self, 1), text='Floor 1', bd=0, compound=tk.TOP)
         self.button_s_1.pack(side=LEFT)
+
         self.button_s_2 = tk.Button(self.leftpanel_switch, state=NORMAL, command=lambda:
         floor_switch(self, 2), text='Floor 2', bd=0, compound=tk.TOP)
         self.button_s_3 = tk.Button(self.leftpanel_switch, state=NORMAL, command=lambda:
-        refresh(self), text='Refresh', bd=0, compound=tk.TOP)
+        floor_switch(self, 3), text='Floor 3', bd=0, compound=tk.TOP)
         self.button_s_3.pack(side=RIGHT)
         self.button_s_2.pack(side=RIGHT)
 
@@ -125,18 +128,21 @@ class Map(tk.Frame):
         self.f = Figure(figsize=(5, 5), dpi=100)
         self.canvas = FigureCanvasTkAgg(self.f, self)
         self.cur_selection = None
+        self.onfloor_total = Label(self, justify=LEFT, font="Arial 16", fg="#bc5151",anchor=S)
 
     def parse(self):
         globals()
         self.f = Figure(figsize=(5, 5), dpi=100)
-        self.a = self.f.add_subplot(111)
+        self.a = self.f.add_subplot(111, title="")
         self.a.imshow(self.img, extent=[0, 1550, 770, 0])
         data = self.session.get(hostname + query_active).json()
         list = Listbox(self.leftpanel, bd=2, bg='#9DBFC0', width=27,
             font="Helvetica 16 bold italic", fg = "#3c8081")
-        info_label = Label(self, justify=LEFT, font="Arial 16", fg="#bc5151")
+        info_label = Label(self, justify=LEFT, font="Arial 16", fg="#bc5151",anchor=SW)
         typed_text = self.ment.get()
-
+        onfloor_total = Label(self.leftpanel, justify=LEFT, font="Arial 16", fg="#3c8081",anchor=S)
+        onfloor = 0
+        total = 0
         for mac in data:
             if self.listbox.curselection() and (mac['macAddress'] in str(self.listbox.get((self.listbox.curselection())))
                         or (mac['userName'] in str(self.listbox.get(self.listbox.curselection())) and mac['userName'])):
@@ -152,17 +158,26 @@ class Map(tk.Frame):
                 if not self.listbox.curselection():
                     self.a.plot(mac['mapCoordinate']['x'], mac['mapCoordinate']['y'], 'ro', markersize=8)
                     self.a.text(mac['mapCoordinate']['x'], mac['mapCoordinate']['y'], mac['userName'], fontsize=8)
+                onfloor += 1
+            if mac['ipAddress']:
+                total += 1
             if not mac['macAddress'] in self.connected and mac['ipAddress']:
                 self.connected += mac['macAddress']
-                if " " in self.connected and '1' in mac['mapInfo']['mapHierarchyString']:
+                if  " " in self.connected and '1' in mac['mapInfo']['mapHierarchyString']:
                     messagebox.showinfo("Notification", mac['macAddress'] + " " + mac['userName'] + " now is on the first floor")
-                elif " " in self.connected:
-                    messagebox.showinfo("Notification", mac['macAddress'] + " " + mac['userName'] + "now is on the second floor")
+                elif " " in self.connected and '2' in mac['mapInfo']['mapHierarchyString']:
+                    messagebox.showinfo("Notification", mac['macAddress'] + " " + mac['userName'] + " now is on the second floor")
+                elif " " in self.connected and '3' in mac['mapInfo']['mapHierarchyString']:
+                    messagebox.showinfo("Notification", mac['macAddress'] + " " + mac['userName'] + " now is on the third floor")
         self.connected += " "
-
-        if (self.listbox.curselection()):
-            list.selection_set(self.listbox.curselection())
-
+        onfloor_total['text'] = "On floor: " + str(onfloor) + "/ total: " + str(total)
+        self.onfloor_total.pack_forget()
+        self.onfloor_total = onfloor_total
+        # self.onfloor_total.pack(side=tk.BOTTOM)
+        self.onfloor_total.pack(side=tk.TOP)
+        if self.listbox.curselection():
+            if str(self.listbox.get(self.listbox.curselection())) in str(list.get(self.listbox.curselection())):
+                list.selection_set(self.listbox.curselection())
         if list:
             self.listbox.pack_forget()
             self.listbox = list
@@ -174,9 +189,13 @@ class Map(tk.Frame):
             self.canvas._tkcanvas.pack_forget()
             self.canvas = FigureCanvasTkAgg(self.f, self)
             self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
         if not self.listbox.curselection():
             self.info_label.pack_forget()
-
+        else:
+            self.info_label.pack_forget()
+            self.info_label = info_label
+            self.info_label.pack(anchor=SW)
 
 class Presense(tk.Frame):
 
@@ -205,24 +224,27 @@ def close_window():
 
 
 def label_paste(info_label, mac):
-    info_label['text'] = "\n\n\n\nMacAddress:\t\t " + mac['macAddress'] + "\nFloor:\t\t\t "
+    info_label['text'] = "\tMacAddress:\t\t " + mac['macAddress'] + "\n\tFloor:\t\t\t "
     if '1st_Floor' in mac['mapInfo']['mapHierarchyString']:
-        info_label['text'] += "1st_Floor"
+        info_label['text'] += "First floor"
+    elif '2nd_Floor' in mac['mapInfo']['mapHierarchyString']:
+        info_label['text'] += "Second floor"
     else:
-        info_label['text'] += "2nd_Floor"
-    info_label['text'] += "\nRSSI:\t\t\t " + \
-        str(mac['statistics']['maxDetectedRssi']['rssi']) + "\nSSID:\t\t\t " + \
-        mac['ssId'] + "\nDevice type:\t\t " + str(mac['manufacturer']) + \
-        "\nUserName:\t\t " + mac['userName'] + "\nFirst Located Time:\t " + \
-        mac['statistics']['firstLocatedTime'] + "\nLast Located Time:\t\t " + \
-        mac['statistics']['lastLocatedTime']
-
+        info_label['text'] += "Third floor"
+    info_label['text'] += "\n\tRSSI:\t\t\t " + \
+        str(mac['statistics']['maxDetectedRssi']['rssi']) + "\n\tSSID:\t\t\t " + \
+        mac['ssId'] + "\n\tDevice type:\t\t " + str(mac['manufacturer']) + \
+        "\n\tUserName:\t\t " + mac['userName'] + "\n\tFirst Located Time:\t " + \
+        mac['statistics']['firstLocatedTime'] + "\n\tLast Located Time:\t\t " + \
+        mac['statistics']['lastLocatedTime'] + "\n\n\n"
 
 def floor_switch(self, cont):
     if cont == 1:
         self.img = plt.imread("Perks/e1.png")
-    else:
+    elif cont == 2:
         self.img = plt.imread("Perks/e2.png")
+    else:
+        self.img = plt.imread("Perks/e3.png")
     self.floor_switch = cont
     if self.listbox.curselection():
         self.listbox.selection_clear(self.listbox.curselection())
